92f2bc3d89548c27aa95e1b517882685a68b80c5
JonasHiltl <jonashiltl2003@gmail.com>
2020-12-25 15:41:27 +0100

    start express api

--- web-app/application/javascript/app.js
+++ web-app/application/javascript/app.js
@@ -2,3 +2,10 @@
 
 const { Gateway, Wallets } = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
+const path = require('path');
+
+const channelName = 'mychannel';
+const mspOrg1 = 'Org1MSP';
+const walletPath = path.join(__dirname, '..', 'wallet');
+
+console.log(walletPath)
