6e92af30ea619ec4142a698c35ddd945d5eef9f2
JonasHiltl <jonashiltl2003@gmail.com>
2020-12-26 18:53:15 +0100

    new gitignore

--- web-app/application/javascript/app.js
+++ web-app/application/javascript/app.js
@@ -7,6 +7,6 @@ const path = require('path');
 const channelName = 'mychannel';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, '..', 'wallet');
-const ccpPath = path.resolve(__dirname, '..',  'connection-org1.yaml');
+const ccpPath = path.resolve(__dirname, '..', '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
 
 console.log(ccpPathv2)
