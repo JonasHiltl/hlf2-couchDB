e7c35180514c9e05cbd0d4d162d9c499bedd011e
JonasHiltl <jonashiltl2003@gmail.com>
2020-12-26 16:15:12 +0100

    successfully enrolled admin and "appUser"

--- web-app/application/javascript/app.js
+++ web-app/application/javascript/app.js
@@ -7,5 +7,6 @@ const path = require('path');
 const channelName = 'mychannel';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, '..', 'wallet');
+const ccpPath = path.resolve(__dirname, '..',  'connection-org1.yaml');
 
-console.log(walletPath)
+console.log(ccpPathv2)
