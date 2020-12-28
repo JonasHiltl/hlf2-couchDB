cb0894f3d057f4c76108c985ee6283f47d5abeac
JonasHiltl <jonashiltl2003@gmail.com>
2020-12-29 00:39:50 +0100

    first steps for registering the user

--- web-app/application/javascript/server.js
+++ web-app/application/javascript/server.js
@@ -1,4 +1,5 @@
 'use strict';
+require("dotenv").config();
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,11 +12,21 @@ const fs = require('fs');
 
 const channelName = 'mychannel';
 const mspOrg1 = 'Org1MSP';
+const adminName = process.env.adminName;
 const walletPath = path.join(__dirname, '..', 'wallet');
 const ccpPath = path.resolve(__dirname, '..', 'connection-org1.json');
 
-//register
+//need to save the information off-chain in postgres
+// either create UUID in react or generate it in server and also save the information in postgres in the same register function
 app.post('/api/register', async function (req, res) {
+    var UUID = req.body.UUID
+    // var firstName = req.body.firstName;
+    // var lastName = req.body.lastName;
+    // var username = firstName + ' ' + lastName;
+    // console.log('Your username is ' + username)
+    // var email = req.body.email;
+    // console.log('Your email is ' + email)
+    // var password = req.body.password
     try{
 
         // Create a new file system based wallet for managing identities.
@@ -23,13 +34,26 @@ app.post('/api/register', async function (req, res) {
         const wallet = new FileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
 
+        const userIdentity = await wallet.get(UUID)
+        if (userIdentity) {
+            console.log('An Identity for this "UUID" already exists in the wallet')
+            var response = {
+                success: false,
+                message: 'You already registered successfully'
+            };
+            return response
+        }
+
+        const adminIdentity = await wallet.get(adminName)
+
     } catch (error)  {
 
     }
 });
 
-//login
 app.post('/api/login', async function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password
     try{
 
         // Create a new file system based wallet for managing identities.
